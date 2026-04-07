aace66c04eff760694fa2493d089152c25375f6b
Gustavo Kimura <gustavokimuraf@gmail.com>
2026-04-07 09:50:42 -0300

    feat: add HTTP properties to TransactionResource

--- backend/src/main/java/com/findash/TransactionResource.java
+++ backend/src/main/java/com/findash/TransactionResource.java
@@ -1,15 +1,22 @@
 package com.findash;
 
+import java.util.List;
+import java.util.UUID;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
-import java.util.List;
-import java.util.UUID;
 
 @Path("/transactions")
 @ApplicationScoped
